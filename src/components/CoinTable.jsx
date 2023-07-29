4ffad0cc790536de1ba6bc6a5e6caf092677f92f
BikashKakati <bikashkakati796@gmail.com>
2023-07-29 23:47:23 +0530

    Working on CoinTable
    Coin table is on progress
    - Head is Completed

--- src/components/CoinTable.jsx
+++ src/components/CoinTable.jsx
@@ -1,8 +1,50 @@
+import { Container, Table, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from '@mui/material'
 import React from 'react'
 
 const CoinTable = () => {
+  const stylesFor = {
+    coinTableContainer:{
+      textAlign:"center",
+      marginTop:15,
+    },
+    headLine:{
+      fontSize: 35,
+      fontWeight: 700,
+    },
+    searchBar:{
+      width:"100%",
+      marginTop:3,
+      marginBottom:3,
+    }
+  }
   return (
-    <div>CoinTable</div>
+    <Container sx={stylesFor.coinTableContainer}>
+      <Typography sx={stylesFor.headLine}> 
+        Cryptocurrency Prices By Market Cap
+      </Typography>
+      <TextField
+      label="Search Coins.."
+      variant="outlined"
+      sx={stylesFor.searchBar}
+      />
+      <TableContainer>
+        <Table>
+          <TableHead sx={{background:"gold"}}>
+            <TableRow>
+              {["Coin", "Price", "24h Change", "Market Cap"].map((head)=>(
+                <TableCell
+                sx={{ color:"#000",}}
+                key={head}
+                align ={head === "Coin" ?"":"right"}
+                >
+                  {head}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+        </Table>
+      </TableContainer>
+    </Container>
   )
 }
 
