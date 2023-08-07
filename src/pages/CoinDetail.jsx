4447d243aa156603fabb1c0cd4ba43c0ee1b2e6c
Bikash Kakati <bikashkakati796@gmail.com>
2023-08-07 09:07:02 +0530

    Starting coins details page

--- src/pages/CoinDetail.jsx
+++ src/pages/CoinDetail.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { useFetch } from '../hooks/useFetch';
 
 const CoinDetail = () => {
+  const {id:coinId} = useParams();
+  const {apiData:coinDetails, loading} = useFetch(coinId);
   return (
     <div>CoinDetail</div>
   )
