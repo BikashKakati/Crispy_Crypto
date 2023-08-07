f1492833c4ed485af4ef70b99834beb5cfcf3561
Bikash Kakati <bikashkakati796@gmail.com>
2023-08-07 09:05:53 +0530

    Adding react-router-dom
    Now we have two routes
    - Home
    - Coindetails

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import './App.css'
 import { ThemeProvider, createTheme } from '@mui/material'
+import CoinDetail from './pages/CoinDetail';
 
 function App() {
   const darkTheme = createTheme({
-    palette:{
-      primary:{
+    palette: {
+      primary: {
         main: "#fff",
       },
       mode: "dark",
@@ -16,10 +18,16 @@ function App() {
 
   return (
     <ThemeProvider theme={darkTheme}>
-    <>
-    <Navbar/>
-    <Home/>
-    </>
+      <>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/details/:id" element={<CoinDetail/>} />
+          </Routes>
+        </Router>
+
+      </>
     </ThemeProvider>
   )
 }
