25a6fd3751adbb3b43e76356d8c90e5350c4a0c2
Bikash Kakati <bikashkakati796@gmail.com>
2023-08-18 00:45:57 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Carousel from '../components/carousel';
+import React from 'react';
+import Carousel from "../components/Carousel";
 import CoinTable from '../components/CoinTable';
 const Home = () => {
     return (
